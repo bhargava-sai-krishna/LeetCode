605cae30ffc6931c6209cbfcf2f151748b15238d
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2024-09-08 21:23:35 +0530

    Time: 0 ms (100%), Space: 43.4 MB (34.41%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -9,33 +9,38 @@
  * }
  */
 class Solution {
+
     public ListNode[] splitListToParts(ListNode head, int k) {
-        ListNode[] list=new ListNode[k];
-        ListNode currNode=head;
-        int len=0;
-        while(currNode!=null)
-        {
-            currNode=currNode.next;
-            len=len+1;
+        ListNode[] ans = new ListNode[k];
+        int size = 0;
+        ListNode current = head;
+        while (current != null) {
+            size++;
+            current = current.next;
         }
-        int rem=len%k, req=len/k;
-        currNode=head;
-        for(int i=0;currNode!=null;i++)
-        {
-            int count=req+(rem>0?1:0);rem=rem-1;
-            list[i]=currNode;
-            for(;count>1;count--)
-            {
-                currNode=currNode.next;
+        int splitSize = size / k;
+        int numRemainingParts = size % k;
+        current = head;
+        ListNode prev = current;
+        for (int i = 0; i < k; i++) {
+            ListNode newPart = current;
+            int currentSize = splitSize;
+            if (numRemainingParts > 0) {
+                numRemainingParts--;
+                currentSize++;
             }
-            if(currNode==null)
-            {
-                break;
+            int j = 0;
+            while (j < currentSize) {
+                prev = current;
+                current = current.next;
+                j++;
             }
-            ListNode temp=currNode.next;
-            currNode.next=null;
-            currNode=temp;
+            if (prev != null) {
+                prev.next = null;
+            }
+
+            ans[i] = newPart;
         }
-        return list;
+        return ans;
     }
 }
