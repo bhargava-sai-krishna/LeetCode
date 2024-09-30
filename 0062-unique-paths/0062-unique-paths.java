acae6f49c5ab98a63ccfe8dd6f8ea816df90e3f9
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2024-09-30 20:01:33 +0530

    Time: 0 ms (100%), Space: 40.6 MB (27.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long result = 1;
+        int k = (m-1 < n-1) ? m-1 : n-1;
+        for (int i=1; i<=k; i++) {
+            result = result * (m + n - 2 - i + 1) / i;
+        }
+        return (int)result;
+    }
+}
