2eef65b2847b96161ec2ece7aa1b8b329d9815db
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2025-09-15 22:02:29 +0530

    Time: 2 ms (91.65%), Space: 42.6 MB (39.44%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,16 +1,26 @@
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
-        String[] words=text.split(" ");
-        int count=words.length;
-        for(String s:words){
-            boolean possible=true;
-            for(char c:brokenLetters.toCharArray()){
-                if(s.indexOf(c)!=-1){
-                    possible=false;
+        String[] words = text.split(" ");
+        Set<Character> brokenSet = new HashSet<>();
+        
+        // Populate the HashSet with broken characters for O(1) lookup
+        for (char c : brokenLetters.toCharArray()) {
+            brokenSet.add(c);
+        }
+        
+        int count = 0;
+        
+        // Iterate through each word and check if it can be typed
+        for (String word : words) {
+            boolean canBeTyped = true;
+            for (char c : word.toCharArray()) {
+                if (brokenSet.contains(c)) {
+                    canBeTyped = false;
+                    break; 
                 }
             }
-            if(!possible){
-                count--;
+            if (canBeTyped) {
+                count++;
             }
         }
         return count;
