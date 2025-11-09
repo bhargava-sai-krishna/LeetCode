8868bbf8ec937017c6276b52c7c811e3eaf4dd24
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2025-11-09 19:31:55 +0530

    Time: 0 ms (100%), Space: 43.3 MB (8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zer=0, one=0;
+        int z=0, o=0;
         for(int i:nums){
             switch(i){
                 case 0:
-                    zer++;
+                    z++;
                     break;
                 case 1:
-                    one++;
-                    break;
-                case 2:
+                    o++;
                     break;
             }
         }
         for(int i=0;i<nums.length;i++){
-            if(zer!=0){
+            if(z!=0){
                 nums[i]=0;
-                zer--;
+                z--;
             }
-            else if(one!=0){
+            else if(o!=0){
                 nums[i]=1;
-                one--;
+                o--;
             }
             else{
                 nums[i]=2;
