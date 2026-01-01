f3a7e75a88f1c8b314570f1c27360d6396a27bc2
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-01-01 15:54:57 +0530

    Time: 16 ms (5.7%), Space: 187.1 MB (5.7%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Queue<Integer> pos=new LinkedList<>();
-        Queue<Integer> neg=new LinkedList<>();
+        Queue<Integer> posStack=new LinkedList<>();
+        Queue<Integer> negStack=new LinkedList<>();
         for(int i:nums){
-            if(i>0){
-                pos.add(i);
+            if(i>=0){
+                posStack.add(i);
             }
             else{
-                neg.add(i);
+                negStack.add(i);
             }
         }
         for(int i=0;i<nums.length;i++){
             if(i%2==0){
-                nums[i]=pos.poll();
+                nums[i]=posStack.poll();
             }
             else{
-                nums[i]=neg.poll();
+                nums[i]=negStack.poll();
             }
         }
         return nums;
