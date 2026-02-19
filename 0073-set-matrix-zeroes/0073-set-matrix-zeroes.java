1add277964a210510957be9197a761dbd3b39037
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-02-19 15:39:31 +0530

    Time: 0 ms (100%), Space: 46.9 MB (98.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,48 +1,36 @@
 class Solution {
-    public void setZeroes(int[][] mat) {
-        int rows = mat.length;
-        int cols = mat[0].length;
-        boolean fcol = false;
-        boolean frow = false;
-        for (int i = 0; i < rows; i++) {
-            if (mat[i][0] == 0) {
-                fcol = true;
-                break;
-            }
-        }
-        for (int i = 0; i < cols; i++) {
-            if (mat[0][i] == 0) {
-                frow = true;
-                break;
-            }
-        }
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    mat[0][j] = 0;
-                    mat[i][0] = 0;
+    public void setZeroes(int[][] matrix) {
+        boolean overlap=false;
+        int n=matrix.length, m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    if(i>0){
+                        matrix[i][0]=0;
+                    }
+                    else{
+                        overlap=true;
+                    }
                 }
             }
         }
-        for (int i = 1; i < rows; i++) {
-            if (mat[i][0] == 0) {
-                Arrays.fill(mat[i], 0);
-            }
-        }
-        for (int j = 1; j < cols; j++) {
-            if (mat[0][j] == 0) {
-                for (int i = 1; i < rows; i++) {
-                    mat[i][j] = 0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
-        if (fcol) {
-            for (int i = 0; i < rows; i++) {
-                mat[i][0] = 0;
+        if(matrix[0][0]==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
-        if (frow) {
-            Arrays.fill(mat[0], 0);
+        if(overlap){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
         }
     }
 }
