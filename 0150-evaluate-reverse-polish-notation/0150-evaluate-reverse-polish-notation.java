efc545239a10f142579a873543d6a14530c453bc
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-02-23 12:15:08 +0530

    Time: 7 ms (43.12%), Space: 47 MB (8.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,49 +1,31 @@
 class Solution {
+    private static String performOperation(int a, int b, String operation) {
+        return switch(operation) {
+            case "+" -> String.valueOf(a + b);
+            case "-" -> String.valueOf(a - b);
+            case "*" -> String.valueOf(a * b);
+            case "/" -> String.valueOf(a / b);
+            default -> String.valueOf(a + b);
+        };
+    } 
+
     public int evalRPN(String[] tokens) {
-        Stack<String> s1=new Stack<>();
-        int a, b, res=0;
-        String o1, o2, r="";
-        if(tokens.length==0){
-            return 0;
-        }
+        Stack<String> stack = new Stack<>();
+        int ans = 0;
 
-        for(String s:tokens){
-            if(!s1.empty() && (s.equals("+")||s.equals("-")||s.equals("/")||s.equals("*"))){
-                o1=s1.pop();
-                o2=s1.pop();
-                b=Integer.parseInt(o1);
-                a=Integer.parseInt(o2);
-
-                if(s.equals("+")){
-                    res=a+b;
-                }
-
-                else if(s.equals("-")){
-                    res=a-b;
-                }
-                
-                else if(s.equals("/")){
-                    if(b==0){
-                        return -1;
-                    }
-                    else{
-                        res=a/b;
-                    }
-                }
-
-                else{
-                    res=a*b;
-                }
-
-                r=String.valueOf(res);
-                s1.push(r);
-            }
-
-            else{
-                s1.push(s);
+        for(String s : tokens) {
+            if(!s.equals("+") && 
+                !s.equals("-") && 
+                !s.equals("*") && 
+                !s.equals("/")) {
+                stack.push(s);
+            } else {
+                int a = Integer.parseInt(stack.pop());
+                int b = Integer.parseInt(stack.pop());
+                stack.push(performOperation(b, a, s));
             }
         }
 
-        return Integer.parseInt(s1.pop());
+        return Integer.parseInt(stack.pop());
     }
 }
