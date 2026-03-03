de7e02dde56ec79242a33420178e37cd50a78bb8
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-03 09:09:32 +0530

    Time: 27 ms (41.97%), Space: 48.5 MB (35.53%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,23 +1,43 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] taskGroup=new int[26];
-        int max=0;
-        int maxCount=0;
-        for(char c:tasks){
-            taskGroup[c-'A']+=1;
-            if(max==taskGroup[c-'A']){
-                maxCount++;
-            }
-            else if(max<taskGroup[c-'A']){
-                max=taskGroup[c-'A'];
-                maxCount=1;
+        int cycles = 0;
+        int[] frequency = new int[26];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(char task :  tasks) {
+            frequency[task - 'A']++;
+        }
+
+        for(int i : frequency) {
+            if(i > 0) {
+                pq.offer(i);
             }
         }
-        int partCount=max-1;
-        int partLength=n-(maxCount-1);
-        int emptySlots=partCount*partLength;
-        int avaliable=tasks.length-max*maxCount;
-        int idl=Math.max(0, emptySlots-avaliable);
-        return tasks.length+idl;
+
+        while(!pq.isEmpty()) {
+            List<Integer> remain = new ArrayList<>();
+            int currCycles = n + 1; 
+
+            while(currCycles > 0 && !pq.isEmpty()) {
+                int maxFreq = pq.poll();
+                if(maxFreq > 1) {
+                    remain.add(maxFreq - 1);
+                }
+                cycles++;
+                currCycles--;
+            }
+
+            for(int count : remain) {
+                pq.offer(count);
+            }
+
+            if(pq.isEmpty()) {
+                break;
+            }
+
+            cycles += currCycles;
+        }
+
+        return cycles;
     }
 }
