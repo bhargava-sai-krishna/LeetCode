0d5fdd94850c0a3de93d0e7eaf20cd2bb2f1d5f7
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-03 15:15:26 +0530

    Time: 1 ms (99.68%), Space: 48.9 MB (71.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,51 +2,35 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int[] arr = new int[m + n];
-        int i = 0, j = 0, k = 0;
-        if (m==0&&n==0) 
-        {
-            return 0;
-        }
-        while (i < n && j < m) 
-        {  
-            if (nums1[i] <= nums2[j]) 
-            {
-                arr[k] = nums1[i];
-                i = i + 1;
-            } 
-            else 
-            {
-                arr[k] = nums2[j];
-                j = j + 1;
+        int[] combined = new int[m + n];
+
+        int k = 0, i = 0, j = 0;
+
+        while(i < n && j < m) {
+            if(nums1[i] > nums2[j]) {
+                combined[k] = nums2[j];
+                j++;
+            } else {
+                combined[k] = nums1[i];
+                i++;
             }
-            k = k + 1;
+            k++;
         }
 
-        while (i < n) 
-        {  
-            arr[k] = nums1[i];
-            i = i + 1;
-            k = k + 1;
+        while (i < n) {
+            combined[k++] = nums1[i++];
         }
 
-        while (j < m) 
-        { 
-            arr[k] = nums2[j];
-            j = j + 1;
-            k = k + 1;
+        while (j < m) {
+            combined[k++] = nums2[j++];
         }
 
-        if ((m+n)%2!=0) 
-        {
-            return arr[(m+n)/2]; 
-        } 
-        else 
-        {
-            int temp=(m+n)/2;
-            double a = arr[temp];
-            double b = arr[temp - 1];
-            return (a + b) / 2.0;
+        int len = m + n;
+
+        if(len % 2 == 0) {
+            return (combined[len / 2] + combined[len / 2 - 1]) / 2.0;
+        } else {
+            return combined[(m + n) / 2];
         }
     }
 }
