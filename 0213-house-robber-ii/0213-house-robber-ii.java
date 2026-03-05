56581f99b439f05d0233c37a0653ced170cae36e
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-05 12:57:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     private static int solve(int[] nums, int start, int end) {
-        int n = end - start + 1;
-        int[] dp = new int[n];
-        dp[0] = nums[start];
-        dp[1] = Math.max(nums[start], nums[start + 1]);
 
-        for(int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i + start]);
+        int currTake = 0;
+        int currNotTake = 0;
+
+        for(int i = start; i <= end; i++) {
+            int curr = Math.max(currTake, currNotTake + nums[i]);
+            currNotTake = currTake;
+            currTake = curr;
         }
 
-        return dp[n - 1];
-
+        return currTake;
     }
     public int rob(int[] nums) {
         if(nums.length == 1) {
