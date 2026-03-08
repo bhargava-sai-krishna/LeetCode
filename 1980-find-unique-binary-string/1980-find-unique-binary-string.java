a9ef865948531bcb63c0fdcd6a5d15dd8e4b32f0
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-08 09:23:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,33 +1,12 @@
 class Solution {
-    private String makeToSutialbleLength(String s, int n) {
-        if(s.length() == n) {
-            return s;
-        }
-
-        while(s.length() != n) {
-            s += "0";
-        }
-
-        return s;
-    }
-
     public String findDifferentBinaryString(String[] nums) {
         int n = nums.length;
-        int maxPossibleNumber = (int)Math.pow(2, n) - 1;
-        Set<String> set = new HashSet<>();
+        char[] res = new char[n];
 
-        for(String s : nums) {
-            set.add(s);
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[i].charAt(i) == '0' ? '1' : '0';
         }
 
-        for(int i = 0; i <= maxPossibleNumber; i++) {
-            String s = makeToSutialbleLength(Integer.toBinaryString(i), n);;
-            
-            if(!set.contains(s)) {
-                return s;
-            }
-        }
-
-        return new String();
+        return new String(res);
     }
 }
