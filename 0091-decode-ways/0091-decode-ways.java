6fab73ac19f911ce68cb71263666cb1e88d456e0
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-17 14:48:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,23 @@
-public class Solution {
+class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+        int n = s.length();
+        if(s.isEmpty() || n == 0 || s.charAt(0) == '0') {
             return 0;
         }
 
-        int n = s.length();
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
 
-        for (int i = 2; i <= n; ++i) {
-            int oneDigit = s.charAt(i - 1) - '0';
-            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+        for(int i = 2; i <= n; i++) {
+            int ones = s.charAt(i - 1) - '0';
+            int tens = Integer.parseInt(s.substring(i - 2, i));
 
-            if (oneDigit != 0) {
+            if(ones != 0) {
                 dp[i] += dp[i - 1];
             }
 
-            if (10 <= twoDigits && twoDigits <= 26) {
+            if(tens >= 10 && tens <= 26) {
                 dp[i] += dp[i - 2];
             }
         }
