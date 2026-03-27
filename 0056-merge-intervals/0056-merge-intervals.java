3ce7b81a2a649af3a7be5e54031aeebbf10b12e2
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-03-27 10:22:55 +0530

    Time: 18 ms (5.29%), Space: 48.4 MB (98.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
-        List<int[]> ans=new ArrayList<>();
-        int[] prev=intervals[0];
-        for(int i=0;i<intervals.length;i++){
-            int[] temp=intervals[i];
-            if(temp[0]<=prev[1]){
-                prev[1]=Math.max(prev[1],temp[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            Comparator.comparingInt((int[] a) -> a[0])
+              .thenComparingInt(a -> a[1])
+        );
+
+        for(int[] interval : intervals) {
+            pq.offer(interval);
+        }
+
+        List<int[]> ans = new ArrayList<>();
+
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+
+            if(ans.isEmpty()) {
+                ans.add(curr);
+                continue;
             }
-            else{
-                ans.add(prev);
-                prev=temp;
+
+            int[] prev = ans.get(ans.size() - 1);
+
+            if(curr[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], curr[1]);
+            } else{
+                ans.add(curr);
             }
         }
-        ans.add(prev);
+
         return ans.toArray(new int[ans.size()][]);
     }
 }
