d2d3b50f6f6ca0af335d8aceaaded5e66a5a1d64
bhargava-sai-krishna <100348267+bhargava-sai-krishna@users.noreply.github.com>
2026-05-05 09:25:08 +0530

    Time: 0 ms (100%), Space: 44.4 MB (25.1%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,43 +10,29 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0) 
-        {
-            return head;
-        }
-        int length = 1; 
-        ListNode currNode1=head;
-        while(currNode1.next != null) 
-        {
-            length++;
-            currNode1 = currNode1.next;
-        }
-        k = k % length;
-        if(k == 0) 
-        {
-            return head; 
-        }
-        currNode1=head;
-        ListNode currNode2=head;
-        int i=0;
-        while(currNode1.next!=null)
-        {
-            if(i>=k)
-            {
-                currNode2=currNode2.next;
-            }
-            currNode1=currNode1.next;
-            i=i+1;
-        }
-        currNode1=currNode2.next;
-        currNode2.next=null;
-        ListNode temp=currNode1;
-        while(temp.next!=null)
-        {
-            temp=temp.next;
-        }
-        temp.next=head;
-        head=currNode1;
+    if (head == null || head.next == null) {
         return head;
     }
+
+    ListNode curr = head;
+    int length = 1;
+    while (curr.next != null) {
+        curr = curr.next;
+        length++;
+    }
+    curr.next = head;
+
+    k = k % length;
+    int stepsToNewHead = length - k;
+
+    ListNode newTail = curr;
+    while (stepsToNewHead-- > 0) {
+        newTail = newTail.next;
+    }
+
+    ListNode newHead = newTail.next;
+    newTail.next = null;
+
+    return newHead;
+}
 }
